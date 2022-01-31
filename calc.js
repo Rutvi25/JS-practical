683a4d5cc983e83df0133a06463e3d7dea71108c
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-01-31 22:38:27 +0530

    added nthRoot function & improved UI

--- calc.js
+++ calc.js
@@ -3,29 +3,29 @@ let deg_flag = true;
 let trigo_flag = true;
 let hyp_flag = true;
 let ef_flag = true;
-//col-1 function change on pressing the 2nd button
+// Col-1 function change on pressing the "2nd" button
 function change_func() {
   if (change_flag) {
     document.getElementById("2nd").style.backgroundColor = "rgb(154, 194, 221)";
-    document.getElementById("square").innerHTML = "x<sup>3</sup>";
-    document.getElementById("root").innerHTML = "&#8731;x";
-    document.getElementById("power").innerHTML = "<sup>y</sup>&radic;x";
-    document.getElementById("pow10").innerHTML = "2<sup>x</sup>";
-    document.getElementById("log").innerHTML = "log<sub>2</sub>x";
-    document.getElementById("ln").innerHTML = "e<sup>x</sup>";
+    document.getElementById("square").innerHTML = "&#119909;<sup>3</sup>";
+    document.getElementById("root").innerHTML = "&#8731;&#119909;";
+    document.getElementById("power").innerHTML = "<sup>y</sup>&radic;&#119909;";
+    document.getElementById("pow10").innerHTML = "2<sup>&#119909;</sup>";
+    document.getElementById("log").innerHTML = "log<sub>2</sub>&#119909;";
+    document.getElementById("ln").innerHTML = "e<sup>&#119909;</sup>";
     change_flag = !change_flag;
   } else {
     document.getElementById("2nd").style.backgroundColor = "var(--operator_bg)";
-    document.getElementById("square").innerHTML = "x<sup>2</sup>";
-    document.getElementById("root").innerHTML = "&radic;x";
-    document.getElementById("power").innerHTML = "x<sup>y</sup>";
-    document.getElementById("pow10").innerHTML = "10<sup>x</sup>";
+    document.getElementById("square").innerHTML = "&#119909;<sup>2</sup>";
+    document.getElementById("root").innerHTML = "&radic;&#119909;";
+    document.getElementById("power").innerHTML = "&#119909;<sup>y</sup>";
+    document.getElementById("pow10").innerHTML = "10<sup>&#119909;</sup>";
     document.getElementById("log").innerHTML = "log";
     document.getElementById("ln").innerHTML = "ln";
     change_flag = !change_flag;
   }
 }
-// for 2nd button under trigonomatry functions
+// For 2nd button under trigonomatry functions
 function trigo_second() {
   if (trigo_flag) {
     document.getElementById("second").style.backgroundColor = "rgb(154, 194, 221)";
@@ -55,7 +55,7 @@ function trigo_second() {
     }
   }
 }
-// for hyp button under trigonomatry functions
+// For hyp button under trigonomatry functions
 function trigo_hyp() {
   if (hyp_flag) {
     document.getElementById("hyp").style.backgroundColor = "rgb(154, 194, 221)";
@@ -85,18 +85,18 @@ function trigo_hyp() {
     }
   }
 }
-// function for evaluation:
+// Function for evaluation:
 function evaluate(expression) {
-  let expr = expression.split("");
-  let operand = [];     //operand stack
-  let operator = [];    //operator stack
+  let expr = expression.split("");  
+  let operand = [];     // operand stack
+  let operator = [];    // operator stack
   for (let i = 0; i < expr.length; i++)
   {
     if (expr[i] == " ")
     {
       continue;
     }
-    //for operands
+    // Conditions to push or pop operands
     if (expr[i] >= "0" && expr[i] <= "9" 
        || expr[i] == "." || expr[i] == "-")
     {
@@ -109,7 +109,7 @@ function evaluate(expression) {
       operand.push(parseFloat(num));
       i--;
     }
-    // for operators
+    // Conditions to push or pop operators
     else if (expr[i] == "(")
     {     
       operator.push(expr[i]);     
@@ -141,11 +141,11 @@ function evaluate(expression) {
   while (operator.length > 0) {
     operand.push(calculate(operator.pop(), operand.pop(), operand.pop()));
   }
-  return operand.pop();    //result 
+  return operand.pop();    // result of Evaluate function
 }
-// function to check precedence of operators.
-// it will return true only if the operator 2 
-//precedence is higher than operator1.
+// Function to check precedence of operators.
+/* It will return true only if the operator 2 has 
+    equal or higher precedence than operator1.*/
 function precedence(operator1, operator2)
 {
   if (operator2 == "(" || operator2 == ")" || operator2 == "-")
@@ -162,6 +162,10 @@ function precedence(operator1, operator2)
   {
     return false;
   }
+  if ((operator1 == "%") && (operator2 == "×" || operator2 == "÷"))
+  {
+    return false;
+  }
   if ((operator1 == "×" || operator1 == "÷" || operator1 == "%")
    && (operator2 == "+" || operator2 == "–"))
   {
@@ -172,13 +176,14 @@ function precedence(operator1, operator2)
     return true;
   }
 }
+// Function to calculate string value according to precedence.
 function calculate(operator, b, a)
 {
   switch (operator){
     case "^":
       return a ** b;
     case "√":
-      return b ** (1/a)
+      return nthRoot(b,a)
     case "%":
       return a%b;
     case "+":
@@ -196,36 +201,44 @@ function calculate(operator, b, a)
     }
     return 0;
 }
-// Functionality
+// Function for finding nth root
+function nthRoot(redicant, index) {
+  if (redicant < 0) {
+    if (index % 2 === 1) return -nthRoot(-redicant, index)
+    if (index % 2 === -1) return -1 / nthRoot(-redicant, -index)
+  }
+  return redicant ** (1 / index)
+}
+// Other Functionalities & Event Handlers
 let screen = document.getElementById("screen");
 let history = document.getElementById("history");
 let buttons = Array.from(document.getElementsByTagName("button"));
 var memory = 0;
 buttons.map(button => {
   button.addEventListener("click", (e) => {
-    switch(e.target.innerText){
+    switch (e.target.innerText) {
       case "C":
         screen.value = "";
         history.value = "";
         break;
       case "π":
-        screen.value += Math.PI;   
-        screen.value = screen.value;   
+        screen.value += Math.PI;
+        screen.value = screen.value;
         break;
       case "e":
         screen.value += Math.E;
-        screen.value = screen.value;       
+        screen.value = screen.value;
         break;
       case "⌫":
         screen.value = screen.value.slice(0, -1);
         break;
-      // operators
-      case "mod":    
+      // Operators
+      case "mod":
         history.value += screen.value + "%";
-        screen.value = ""
+        screen.value = "";
         break;
-      case "+":  
-      case "–":  
+      case "+":
+      case "–":
       case "÷":
       case "×":
       case "(":
@@ -233,47 +246,45 @@ buttons.map(button => {
         history.value += screen.value + e.target.innerText;
         screen.value = "";
         break;
+      case "𝑥y":
+        history.value += screen.value + "^";
+        screen.value = "";
+        break;
       case "±":
         screen.value *= -1;
         break;
-      //factorial
+      // Factorial
       case "n!":
         if (screen.value > 0) {
           let fact = 1;
           for (i = 1; i <= screen.value; i++) {
-              fact *= i;
+            fact *= i;
           }
           screen.value = fact;
-        }
-        else if (screen.value == 0)
-          screen.value = 1;
-        else 
-          screen.value = "invalid input";
+        } else if (screen.value == 0) screen.value = 1;
+        else screen.value = "invalid input";
         break;
-      // converts -ve into +ve
-      case "|x|":
-        if(screen.value < 0)
-          screen.value = screen.value*-1  
+      // Converts -ve into +ve
+      case "|𝑥|":
+        if (screen.value < 0) screen.value = screen.value * -1;
         break;
-      //inverse
-      case "1/x":
-        if (screen.value == 0)
-          screen.value = "infinity";
-        else
-          screen.value = 1/screen.value;
+      // Inverse
+      case "1/𝑥":
+        if (screen.value == 0) screen.value = "infinity";
+        else screen.value = 1 / screen.value;
         break;
-      //yth root
-      case "√x":
+      // Root
+      case "√𝑥":
         screen.value = Math.sqrt(screen.value);
         break;
-      case "∛x":
+      case "∛𝑥":
         screen.value = Math.cbrt(screen.value);
         break;
-      case "y√x":
+      case "y√𝑥":
         history.value += screen.value + "√";
-        screen.value = ""
+        screen.value = "";
         break;
-      //functions
+      // Functions
       case "floor":
         screen.value = Math.floor(screen.value);
         break;
@@ -283,183 +294,165 @@ buttons.map(button => {
       case "round":
         screen.value = Math.round(screen.value);
         break;
-      //power
-      case "10x":
+      // Power
+      case "10𝑥":
         screen.value = 10 ** screen.value;
         break;
-      case "2x":
+      case "2𝑥":
         screen.value = 2 ** screen.value;
         break;
-      case "ex":
+      case "e𝑥":
         screen.value = Math.E ** screen.value;
         break;
-      //Trigonometric function
+      case "𝑥3":
+        screen.value = screen.value ** 3;
+        break;
+      case "𝑥2":
+        screen.value = screen.value ** 2;
+        break;  
+      // Trigonometric functions
       case "sin":
         if (deg_flag) {
-          screen.value = Math.sin(screen.value*Math.PI/180).toPrecision(10);
-        }
-        else {
+          screen.value = Math.sin((screen.value * Math.PI) / 180).toPrecision(10);
+        } else {
           screen.value = Math.sin(screen.value).toPrecision(10);
         }
         break;
       case "cos":
         if (deg_flag) {
-          screen.value = Math.cos(screen.value*Math.PI/180).toPrecision(10);
-        }
-        else {
+          screen.value = Math.cos((screen.value * Math.PI) / 180).toPrecision(10);
+        } else {
           screen.value = Math.cos(screen.value).toPrecision(10);
         }
         break;
       case "tan":
         if (deg_flag) {
-          screen.value = Math.tan(screen.value*Math.PI/180).toPrecision(10);
-        }
-        else {
+          screen.value = Math.tan((screen.value * Math.PI) / 180).toPrecision(10);
+        } else {
           screen.value = Math.tan(screen.value).toPrecision(10);
         }
         break;
       case "sin-1":
         if (deg_flag) {
           screen.value = Math.asin(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else {
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.asin(screen.value);
         }
         break;
       case "cos-1":
         if (deg_flag) {
           screen.value = Math.acos(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else{
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.acos(screen.value);
         }
         break;
       case "tan-1":
         if (deg_flag) {
           screen.value = Math.atan(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else{
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.atan(screen.value);
         }
         break;
       case "sinh":
         if (deg_flag) {
-          screen.value = Math.sinh(screen.value*Math.PI/180);
-        }
-        else {
+          screen.value = Math.sinh((screen.value * Math.PI) / 180);
+        } else {
           screen.value = Math.sinh(screen.value);
         }
         break;
       case "cosh":
         if (deg_flag) {
-          screen.value = Math.cosh(screen.value*Math.PI/180);
-        }
-        else {
+          screen.value = Math.cosh((screen.value * Math.PI) / 180);
+        } else {
           screen.value = Math.cosh(screen.value);
         }
         break;
       case "tanh":
         if (deg_flag) {
-          screen.value = Math.tanh(screen.value*Math.PI/180);
-        }
-        else {
+          screen.value = Math.tanh((screen.value * Math.PI) / 180);
+        } else {
           screen.value = Math.tanh(screen.value);
         }
         break;
       case "sin-1h":
         if (deg_flag) {
           screen.value = Math.asinh(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else {
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.asinh(screen.value);
         }
         break;
       case "cos-1h":
         if (deg_flag) {
           screen.value = Math.acosh(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else {
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.acosh(screen.value);
         }
         break;
       case "tan-1h":
         if (deg_flag) {
           screen.value = Math.atanh(screen.value);
-          screen.value = screen.value*180/Math.PI;
-        }
-        else{
+          screen.value = (screen.value * 180) / Math.PI;
+        } else {
           screen.value = Math.atanh(screen.value);
         }
         break;
       // Memory Functions
       case "M+":
         document.getElementById("MR").disabled = false;
-        document.getElementById("MC").disabled = false; 
+        document.getElementById("MC").disabled = false;
         memory += parseFloat(screen.value);
-        screen.value = ""
+        screen.value = "";
         break;
       case "M-":
         document.getElementById("MR").disabled = false;
-        document.getElementById("MC").disabled = false; 
+        document.getElementById("MC").disabled = false;
         memory -= parseFloat(screen.value);
-        screen.value = ""
+        screen.value = "";
         break;
       case "MS":
         document.getElementById("MR").disabled = false;
-        document.getElementById("MC").disabled = false; 
-        memory = parseFloat(screen.value)
+        document.getElementById("MC").disabled = false;
+        memory = parseFloat(screen.value);
         break;
       case "MR":
         screen.value = memory;
         break;
       case "MC":
-        memory = 0; 
+        memory = 0;
         document.getElementById("MR").disabled = true;
-        document.getElementById("MC").disabled = true;      
-        break;
-      //power 
-      case "x3":
-        screen.value = screen.value ** 3;    
-        break;
-      case "x2":
-        screen.value = screen.value ** 2;
-        break;
-      case "xy":
-        history.value += screen.value + "^";
-        screen.value = ""
-        break;
-      //logarithm
+        document.getElementById("MC").disabled = true;
+        break;      
+      // Logarithm
       case "exp":
-        var num = evaluate(screen.value);	
+        var num = evaluate(screen.value);
         screen.value = num.toExponential(10);
-        break;	
+        break;
       case "log":
         screen.value = Math.log10(screen.value);
         break;
       case "ln":
-        screen.value = Math.log10(screen.value)/Math.log10(Math.PI);
-        break;  
-      case "log2x":
+        screen.value = Math.log10(screen.value) / Math.log10(Math.PI);
+        break;
+      case "log2𝑥":
         screen.value = Math.log2(screen.value);
         break;
-      // evaluate
+      // Evaluate
       case "=":
         history.value += screen.value;
-        screen.value = ""; 
-        if(ef_flag) {
+        screen.value = "";
+        if (ef_flag) {
           screen.value = evaluate(history.value);
-          history.value = "";        
-        }
-        else {
+          history.value = "";
+        } else {
           screen.value = evaluate(history.value).toExponential(10);
-          history.value = ""; 
+          history.value = "";
         }
-       break;
+        break;
       case "hyp":
       case "2nd":
       case "nd":
@@ -467,9 +460,9 @@ buttons.map(button => {
       // Choice for the mode of calc
       case "DEG":
       case "RAD":
-        if(deg_flag) {
+        if (deg_flag) {
           document.getElementById("deg").innerHTML = "RAD";
-          deg_flag = !deg_flag
+          deg_flag = !deg_flag;
         } else {
           document.getElementById("deg").innerHTML = "DEG";
           deg_flag = !deg_flag;
@@ -477,16 +470,15 @@ buttons.map(button => {
         break;
       // Choice for the mode of calc
       case "F-E":
-        if(ef_flag) {
+        if (ef_flag) {
           document.getElementById("ef").style.background = "rgb(154, 194, 221)";
-          ef_flag = ! ef_flag;    
-        }
-        else {
+          ef_flag = !ef_flag;
+        } else {
           document.getElementById("ef").style.background = "var(--main_bg)";
-          ef_flag = ! ef_flag;
+          ef_flag = !ef_flag;
         }
         break;
-      // default case (for numbers, operators and decimal.)
+      // Default case (for numbers, operators and decimal.)
       default:
         screen.value += e.target.innerText;
     }
