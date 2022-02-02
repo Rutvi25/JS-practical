2578a12ff12654c2da4f510807b46759e3e947eb
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-02-02 18:40:39 +0530

    added the functionality for taking the input using keyboard

--- calc.js
+++ calc.js
@@ -1,141 +1,142 @@
-let change_flag = true; 
-let deg_flag = true;
-let trigo_flag = true;
-let hyp_flag = true;
-let ef_flag = true;
-// Col-1 function change on pressing the "2nd" button
-function change_func() {
-  if (change_flag) {
-    document.getElementById("2nd").style.backgroundColor = "var(--function_change)";
+let calcSecondFlag = true; 
+let degFlag = true;
+let trigonometryInverseFlag = true;
+let trigonometryHyperbolicFlag = true;
+let feFlag = true;
+// Column-1 function change on pressing the "2nd" button.
+function calcSecondBtn() {
+  if (calcSecondFlag) {
+    document.getElementById("calcSecondBtn").style.backgroundColor = "var(--functionChange)";
     document.getElementById("square").innerHTML = "&#119909;<sup>3</sup>";
     document.getElementById("root").innerHTML = "&#8731;&#119909;";
     document.getElementById("power").innerHTML = "<sup>y</sup>&radic;&#119909;";
-    document.getElementById("pow10").innerHTML = "2<sup>&#119909;</sup>";
+    document.getElementById("power10").innerHTML = "2<sup>&#119909;</sup>";
     document.getElementById("log").innerHTML = "log<sub>2</sub>&#119909;";
     document.getElementById("ln").innerHTML = "e<sup>&#119909;</sup>";
-    change_flag = !change_flag;
+    calcSecondFlag = !calcSecondFlag;
   } else {
-    document.getElementById("2nd").style.backgroundColor = "var(--operator_bg)";
+    document.getElementById("calcSecondBtn").style.backgroundColor = "var(--operatorBackground)";
     document.getElementById("square").innerHTML = "&#119909;<sup>2</sup>";
     document.getElementById("root").innerHTML = "&radic;&#119909;";
     document.getElementById("power").innerHTML = "&#119909;<sup>y</sup>";
-    document.getElementById("pow10").innerHTML = "10<sup>&#119909;</sup>";
+    document.getElementById("power10").innerHTML = "10<sup>&#119909;</sup>";
     document.getElementById("log").innerHTML = "log";
     document.getElementById("ln").innerHTML = "ln";
-    change_flag = !change_flag;
+    calcSecondFlag = !calcSecondFlag;
   }
 }
 // For 2nd button under trigonomatry functions
-function trigo_second() {
-  if (trigo_flag) {
-    document.getElementById("second").style.backgroundColor = "var(--function_change)";
-    if (hyp_flag) {
-      document.getElementById("trigo1").innerHTML = "sin<sup>-1</sup>";
-      document.getElementById("trigo2").innerHTML = "cos<sup>-1</sup>";
-      document.getElementById("trigo3").innerHTML = "tan<sup>-1</sup>";
-      trigo_flag = !trigo_flag;
+function trigonometryInverse() {
+  if (trigonometryInverseFlag) {
+    document.getElementById("trigonometryInverse").style.backgroundColor = "var(--functionChange)";
+    if (trigonometryHyperbolicFlag) {
+      document.getElementById("sin").innerHTML = "sin<sup>-1</sup>";
+      document.getElementById("cos").innerHTML = "cos<sup>-1</sup>";
+      document.getElementById("tan").innerHTML = "tan<sup>-1</sup>";
+      trigonometryInverseFlag = !trigonometryInverseFlag;
     } else {
-      document.getElementById("trigo1").innerHTML = "sin<sup>-1</sup>h";
-      document.getElementById("trigo2").innerHTML = "cos<sup>-1</sup>h";
-      document.getElementById("trigo3").innerHTML = "tan<sup>-1</sup>h";
-      trigo_flag = !trigo_flag;
+      document.getElementById("sin").innerHTML = "sin<sup>-1</sup>h";
+      document.getElementById("cos").innerHTML = "cos<sup>-1</sup>h";
+      document.getElementById("tan").innerHTML = "tan<sup>-1</sup>h";
+      trigonometryInverseFlag = !trigonometryInverseFlag;
     }
   } else {
-    document.getElementById("second").style.backgroundColor = "var(--main_bg)";
-    if (hyp_flag) {
-      document.getElementById("trigo1").innerHTML = "sin";
-      document.getElementById("trigo2").innerHTML = "cos";
-      document.getElementById("trigo3").innerHTML = "tan";
-      trigo_flag = !trigo_flag;
+    document.getElementById("trigonometryInverse").style.backgroundColor = "var(--mainBackground)";
+    if (trigonometryHyperbolicFlag) {
+      document.getElementById("sin").innerHTML = "sin";
+      document.getElementById("cos").innerHTML = "cos";
+      document.getElementById("tan").innerHTML = "tan";
+      trigonometryInverseFlag = !trigonometryInverseFlag;
     } else {
-      document.getElementById("trigo1").innerHTML = "sinh";
-      document.getElementById("trigo2").innerHTML = "cosh";
-      document.getElementById("trigo3").innerHTML = "tanh";
-      trigo_flag = !trigo_flag;
+      document.getElementById("sin").innerHTML = "sinh";
+      document.getElementById("cos").innerHTML = "cosh";
+      document.getElementById("tan").innerHTML = "tanh";
+      trigonometryInverseFlag = !trigonometryInverseFlag;
     }
   }
 }
 // For hyp button under trigonomatry functions
-function trigo_hyp() {
-  if (hyp_flag) {
-    document.getElementById("hyp").style.backgroundColor = "var(--function_change)";
-    if (trigo_flag) {
-      document.getElementById("trigo1").innerHTML = "sinh";
-      document.getElementById("trigo2").innerHTML = "cosh";
-      document.getElementById("trigo3").innerHTML = "tanh";
-      hyp_flag = !hyp_flag;
+function trigonometryHyperbolic() {
+  if (trigonometryHyperbolicFlag) {
+    document.getElementById("trigonometryHyperbolic").style.backgroundColor = "var(--functionChange)";
+    if (trigonometryInverseFlag) {
+      document.getElementById("sin").innerHTML = "sinh";
+      document.getElementById("cos").innerHTML = "cosh";
+      document.getElementById("tan").innerHTML = "tanh";
+      trigonometryHyperbolicFlag = !trigonometryHyperbolicFlag;
     } else {
-      document.getElementById("trigo1").innerHTML = "sin<sup>-1</sup>h";
-      document.getElementById("trigo2").innerHTML = "cos<sup>-1</sup>h";
-      document.getElementById("trigo3").innerHTML = "tan<sup>-1</sup>h";
-      hyp_flag = !hyp_flag;
+      document.getElementById("sin").innerHTML = "sin<sup>-1</sup>h";
+      document.getElementById("cos").innerHTML = "cos<sup>-1</sup>h";
+      document.getElementById("tan").innerHTML = "tan<sup>-1</sup>h";
+      trigonometryHyperbolicFlag = !trigonometryHyperbolicFlag;
     }
   } else {
-    document.getElementById("hyp").style.backgroundColor = "var(--main_bg)";
-    if (trigo_flag) {
-      document.getElementById("trigo1").innerHTML = "sin";
-      document.getElementById("trigo2").innerHTML = "cos";
-      document.getElementById("trigo3").innerHTML = "tan";
-      hyp_flag = !hyp_flag;
+    document.getElementById("trigonometryHyperbolic").style.backgroundColor = "var(--mainBackground)";
+    if (trigonometryInverseFlag) {
+      document.getElementById("sin").innerHTML = "sin";
+      document.getElementById("cos").innerHTML = "cos";
+      document.getElementById("tan").innerHTML = "tan";
+      trigonometryHyperbolicFlag = !trigonometryHyperbolicFlag;
     } else {
-      document.getElementById("trigo1").innerHTML = "sin<sup>-1</sup>";
-      document.getElementById("trigo2").innerHTML = "sin<sup>-1</sup>";
-      document.getElementById("trigo3").innerHTML = "sin<sup>-1</sup>";
-      hyp_flag = !hyp_flag;
+      document.getElementById("sin").innerHTML = "sin<sup>-1</sup>";
+      document.getElementById("cos").innerHTML = "sin<sup>-1</sup>";
+      document.getElementById("tan").innerHTML = "sin<sup>-1</sup>";
+      trigonometryHyperbolicFlag = !trigonometryHyperbolicFlag;
     }
   }
 }
 // Function for evaluation of mathematical expression:
 function evaluate(expression) {
-  let expr = expression.split("");  
+  let expressionToken = expression.split("");  
   let operand = [];     // operand stack
   let operator = [];    // operator stack
-  for (let i = 0; i < expr.length; i++)
+  for (let i = 0; i < expressionToken.length; i++)
   {
-    if (expr[i] == " ")
+    if (expressionToken[i] == " ")
     {
       continue;
     }
     // Conditions to push or pop operands
-    if (expr[i] >= "0" && expr[i] <= "9" 
-       || expr[i] == "." || expr[i] == "-")
+    if (expressionToken[i] >= "0" && expressionToken[i] <= "9" 
+       || expressionToken[i] == "." || expressionToken[i] == "-")
     {
       let num = "";
-      while (i < expr.length && expr[i] >= "0" && expr[i] <= "9" || 
-            expr[i] == "." || expr[i] == "-")
+      while (i < expressionToken.length && expressionToken[i] >= "0" && 
+      expressionToken[i] <= "9" || expressionToken[i] == "." || expressionToken[i] == "-")
       {
-        num = num + expr[i++];
+        num = num + expressionToken[i++];
       }
       operand.push(parseFloat(num));
       i--;
     }
     // Conditions to push or pop operators
-    else if (expr[i] == "(")
+    else if (expressionToken[i] == "(")
     {     
-      operator.push(expr[i]);     
+      operator.push(expressionToken[i]);     
     }
-    else if (expr[i] == ")")
+    else if (expressionToken[i] == ")")
     {
-      if (expr[i-2] == "-") {
-        operand.push(expr[i-1]*-1);
+      if (expressionToken[i-2] == "-") {
+        operand.push(expressionToken[i-1]*-1);
       }
       while (operator[operator.length - 1] != "(") {
         operand.push(calculate(operator.pop(), operand.pop(), operand.pop()));
       }
       operator.pop();
     }
-    else if (expr[i] == "+" || expr[i] == "–" 
-    || expr[i] == "×" || expr[i] == "÷" 
-    || expr [i] == "^" || expr[i] == "%" || expr[i] == "√")
+    else if (expressionToken[i] == "+" || expressionToken[i] == "–" 
+    || expressionToken[i] == "×" || expressionToken[i] == "÷" 
+    || expressionToken [i] == "^" || expressionToken[i] == "%" 
+    || expressionToken[i] == "√")
     {
       while (operator.length > 0 
-        && precedence(expr[i], operator[operator.length - 1]))
+        && precedence(expressionToken[i], operator[operator.length - 1]))
       {
         operand.push(
           calculate(operator.pop(), operand.pop(), operand.pop())
         );
       }
-      operator.push(expr[i]);   
+      operator.push(expressionToken[i]);   
 		}
 	}
   while (operator.length > 0) {
@@ -144,30 +145,30 @@ function evaluate(expression) {
   return operand.pop();    // Result of "evaluate" function
 }
 // Function to check precedence of operators.
-/* It will return true only if the operator 2 has 
-    equal or higher precedence than operator1.*/
-function precedence(operator1, operator2)
+/* It will return true only if the second Operator has 
+    equal or higher precedence than first Operator.*/
+function precedence(firstOperator, secondOperator)
 {
-  if (operator2 == "(" || operator2 == ")" || operator2 == "-")
+  if (secondOperator == "(" || secondOperator == ")" || secondOperator == "-")
   {
     return false;
   }  
-  if ((operator1 == "^" || operator1 == "√") 
-  && (operator2 == "+" || operator2 == "–"))
+  else if ((firstOperator == "^" || firstOperator == "√") 
+  && (secondOperator == "+" || secondOperator == "–"))
   {
     return false;
   }
-  if ((operator1 == "^" || operator1 == "√" || operator1 == "÷")
-  && (operator2 == "×" || operator2 == "%"))
+  else if ((firstOperator == "^" || firstOperator == "√" || firstOperator == "÷")
+  && (secondOperator == "×" || secondOperator == "%"))
   {
     return false;
   }
-  if ((operator1 == "%") && (operator2 == "×" || operator2 == "÷"))
+  else if ((firstOperator == "%") && (secondOperator == "×" || secondOperator == "÷"))
   {
     return false;
   }
-  if ((operator1 == "×" || operator1 == "÷" || operator1 == "%")
-   && (operator2 == "+" || operator2 == "–"))
+  else if ((firstOperator == "×" || firstOperator == "÷" || firstOperator == "%")
+   && (secondOperator == "+" || secondOperator == "–"))
   {
     return false;
   }
@@ -177,27 +178,27 @@ function precedence(operator1, operator2)
   }
 }
 // Function to calculate string value according to precedence.
-function calculate(operator, b, a)
+function calculate(operator, secondOperand, firstOperand)
 {
   switch (operator){
     case "^":
-      return a ** b;
+      return firstOperand ** secondOperand;
     case "√":
-      return nthRoot(b,a)
+      return nthRoot(secondOperand,firstOperand)
     case "%":
-      return a%b;
+      return firstOperand % secondOperand;
     case "+":
-      return a + b;
+      return firstOperand + secondOperand;
     case "–":
-      return a - b;
+      return firstOperand - secondOperand;
     case "×":
-      return a * b;
+      return firstOperand * secondOperand;
     case "÷":
-      if (b == 0)
+      if (secondOperand == 0)
       {
         screen.value = "infinity";
       }
-      return parseFloat(a / b);
+      return parseFloat(firstOperand / secondOperand);
     }
     return 0;
 }
@@ -209,11 +210,93 @@ function nthRoot(redicant, index) {
   }
   return redicant ** (1 / index)
 }
+// Input using the keyboard
+document.onkeyup = function(event){
+  switch (event.key) {
+    case "Backspace":
+      document.getElementById("backspace").click();
+      break;
+    case "Delete":
+      document.getElementById("clear").click();
+      break;
+    case "e":
+    case "E":
+      document.getElementById("e").click();
+      break;
+    // operators
+    case "+":
+      document.getElementById("addition").click();
+      break;
+    case "-":
+      document.getElementById("subtraction").click();
+      break;
+    case "*":
+      document.getElementById("multiply").click();
+      break;
+    case "/":
+      document.getElementById("divide").click();
+      break;
+    case "^":
+      document.getElementById("power").click();
+      break;
+    case "%":
+      document.getElementById("mod").click();
+      break;
+    case "(":
+      document.getElementById("openBracket").click();
+      break;
+    case ")":
+      document.getElementById("closeBracket").click();
+      break;
+    case "!":
+      document.getElementById("factorial").click();
+      break;
+    // Result
+    case "=":
+    case "Enter":
+      document.getElementById("equal").click();
+      break;
+    // operands
+    case "1":
+      document.getElementById("1").click();
+      break;
+    case "2":
+      document.getElementById("2").click();
+      break;
+    case "3":
+      document.getElementById("3").click();
+      break;
+    case "4":
+      document.getElementById("4").click();
+      break;
+    case "5":
+      document.getElementById("5").click();
+      break;
+    case "6":
+      document.getElementById("6").click();
+      break;
+    case "7":
+      document.getElementById("7").click();
+      break;
+    case "8":
+      document.getElementById("8").click();
+      break;   
+    case "9":
+      document.getElementById("9").click();
+      break;
+    case "0":
+      document.getElementById("0").click();
+      break;
+    case ".":
+      document.getElementById(".").click();
+      break;       
+  } 
+}
 // Other Functionalities & Event Handlers
 let screen = document.getElementById("screen");
 let history = document.getElementById("history");
 let buttons = Array.from(document.getElementsByTagName("button"));
-var memory = 0;
+let memory = 0;
 buttons.map(button => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
@@ -310,28 +393,28 @@ buttons.map(button => {
         break;  
       // Trigonometric functions
       case "sin":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.sin((screen.value * Math.PI) / 180).toPrecision(10);
         } else {
           screen.value = Math.sin(screen.value).toPrecision(10);
         }
         break;
       case "cos":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.cos((screen.value * Math.PI) / 180).toPrecision(10);
         } else {
           screen.value = Math.cos(screen.value).toPrecision(10);
         }
         break;
       case "tan":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.tan((screen.value * Math.PI) / 180).toPrecision(10);
         } else {
           screen.value = Math.tan(screen.value).toPrecision(10);
         }
         break;
       case "sin-1":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.asin(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -339,7 +422,7 @@ buttons.map(button => {
         }
         break;
       case "cos-1":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.acos(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -347,7 +430,7 @@ buttons.map(button => {
         }
         break;
       case "tan-1":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.atan(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -355,28 +438,28 @@ buttons.map(button => {
         }
         break;
       case "sinh":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.sinh((screen.value * Math.PI) / 180);
         } else {
           screen.value = Math.sinh(screen.value);
         }
         break;
       case "cosh":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.cosh((screen.value * Math.PI) / 180);
         } else {
           screen.value = Math.cosh(screen.value);
         }
         break;
       case "tanh":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.tanh((screen.value * Math.PI) / 180);
         } else {
           screen.value = Math.tanh(screen.value);
         }
         break;
       case "sin-1h":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.asinh(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -384,7 +467,7 @@ buttons.map(button => {
         }
         break;
       case "cos-1h":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.acosh(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -392,7 +475,7 @@ buttons.map(button => {
         }
         break;
       case "tan-1h":
-        if (deg_flag) {
+        if (degFlag) {
           screen.value = Math.atanh(screen.value);
           screen.value = (screen.value * 180) / Math.PI;
         } else {
@@ -443,8 +526,8 @@ buttons.map(button => {
       case "=":
         history.value += screen.value;
         screen.value = "";
-        if (ef_flag) {
-          screen.value = evaluate(history.value);
+        if (feFlag) {
+          screen.value = evaluate(history.value)
           history.value = "";
         } else {
           screen.value = evaluate(history.value).toExponential(10);
@@ -458,22 +541,22 @@ buttons.map(button => {
       // Choice for the mode of calc
       case "DEG":
       case "RAD":
-        if (deg_flag) {
+        if (degFlag) {
           document.getElementById("deg").innerHTML = "RAD";
-          deg_flag = !deg_flag;
+          degFlag = !degFlag;
         } else {
           document.getElementById("deg").innerHTML = "DEG";
-          deg_flag = !deg_flag;
+          degFlag = !degFlag;
         }
         break;
       // Choice for the mode of calc
       case "F-E":
-        if (ef_flag) {
-          document.getElementById("ef").style.background = "var(--function_change)";
-          ef_flag = !ef_flag;
+        if (feFlag) {
+          document.getElementById("fe").style.background = "var(--functionChange)";
+          feFlag = !feFlag;
         } else {
-          document.getElementById("ef").style.background = "var(--main_bg)";
-          ef_flag = !ef_flag;
+          document.getElementById("fe").style.background = "var(--mainBackground)";
+          feFlag = !feFlag;
         }
         break;
       // Default case (for numbers, operators and decimal.)
